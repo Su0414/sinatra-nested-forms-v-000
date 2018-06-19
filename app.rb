0207898a3216fed28fe39aaa3f158f360302281d
Suchitra Naik <susanaik@gmail.com>
2018-06-19 13:29:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ module FormsLab
 
     post '/pirates' do
        raise params.inspect
-      @pirate = Pirate.new(params[:pirate[name]], params[:pirate[weight]], params[:pirate[height]])
+      @pirate = Pirate.new(params[:pirate["name"]], params[:pirate["weight"], params[:pirate["height"]])
 
       @ship1 = Ship.new(params[:ship_name_1], params[:ship_type_1], params[:ship_booty_1])
       @ship1 = Ship.all.first
